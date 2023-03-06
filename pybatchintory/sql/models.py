610973eccb4f4258d341a3eb91cf7189afd48657
Franz Wöllert <franz.woellert@heidelberg.com>
2023-03-06 12:23:38 +0100

    Use BigIntegers for id columns.

--- pybatchintory/sql/models.py
+++ pybatchintory/sql/models.py
@@ -1,8 +1,8 @@
 from typing import Optional
 
 from pybatchintory import config as cfg
-from sqlalchemy import Table, Column, Integer, String, DateTime, Enum, Text, \
-    JSON, MetaData, func
+from sqlalchemy import Table, Column, Integer, String, DateTime, Enum, \
+    JSON, MetaData, func, BigInteger
 from sqlalchemy.engine.base import Engine
 
 TS = func.current_timestamp()
@@ -14,24 +14,25 @@ def generate_inventory_table(name: str,
     return Table(
         name,
         metadata,
-            Column('id', Integer, primary_key=True, autoincrement=True),
-            Column('meta_table', String, nullable=False),
-            Column('job', String, nullable=False),
-            Column('job_identifier', String),
-            Column('job_result', JSON),
-            Column('processing_start', DateTime, nullable=False, default=TS),
-            Column('processing_end', DateTime),
-            Column('meta_id_start', Integer, nullable=False),
-            Column('meta_id_end', Integer, nullable=False),
-            Column('weight', Integer),
-            Column('count', Integer, nullable=False),
-            Column('attempt', Integer, nullable=False, default=1),
-            Column('status',
-                   Enum(*cfg.settings.INVENTORY_STATUS_ENUMS),
-                   nullable=False,
-                   default="running"),
-            Column('logging', String),
-            Column('config', JSON, nullable=False),
+        Column('id', BigInteger, primary_key=True),
+        Column('meta_table', String, nullable=False),
+        Column('job', String, nullable=False),
+        Column('job_identifier', String),
+        Column('job_result', JSON),
+        Column('processing_start', DateTime, nullable=False, default=TS),
+        Column('processing_end', DateTime),
+        Column('meta_id_start', BigInteger, nullable=False),
+        Column('meta_id_end', BigInteger, nullable=False),
+        Column('weight', Integer),
+        Column('count', Integer, nullable=False),
+        Column('attempt', Integer, nullable=False, default=1),
+        Column('status',
+               Enum(*cfg.settings.INVENTORY_STATUS_ENUMS,
+                    name="status_enum"),
+               nullable=False,
+               default="running"),
+        Column('logging', String),
+        Column('config', JSON, nullable=False),
         schema=schema,
         sqlite_autoincrement=True
     )
